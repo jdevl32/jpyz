6f02a1e3a6aa2de4f8de4871813291d3f3626607
jdevl32 <jdevl32@users.noreply.github.com>
2017-02-05 00:58:29 -0500

    cleanup

--- jpyz/expect/common.py
+++ jpyz/expect/common.py
@@ -23,16 +23,9 @@ def expectSendPassword(spawn, password, pattern=None):
     :rtype:
     """
 
-    # jpyzCommon.trace(descriptor="len(password)", value=len(password))
-
     if jpyzCommon.isNull(pattern):
         spawn.waitnoecho()
     else:
-        index = spawn.expect([pattern, pexpect.EOF, pexpect.TIMEOUT])
+        spawn.expect([pattern, pexpect.EOF, pexpect.TIMEOUT])
 
-    bytes = spawn.sendline(password)
-    # index = spawn.expect([pexpect.EOF, pexpect.TIMEOUT])
-    # return index
-    # jpyzCommon.trace(descriptor="bytes", value=bytes)
-    # return not spawn.waitnoecho(timeout=5)
-    return bytes
+    return spawn.sendline(password)
